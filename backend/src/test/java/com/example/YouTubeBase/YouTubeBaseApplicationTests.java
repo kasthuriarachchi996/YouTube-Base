6583e621250499f75e15cd6e7d1dd70232e9fde9
kasthuriarachchi996 <www.pasindudenuka5770@gmail.com>
2023-04-07 08:12:07 +0530

    create project

---
+++ backend/src/test/java/com/example/YouTubeBase/YouTubeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YouTubeBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
